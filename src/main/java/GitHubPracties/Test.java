75ff90baadff3c2c121513b610a844abd3af2f88
ppatil24 <ppatil24@INLEN8520062127.corp.capgemini.com>
2023-02-03 11:38:03 +0530

    I try to add One Element

--- src/main/java/GitHubPracties/Test.java
+++ src/main/java/GitHubPracties/Test.java
@@ -1,5 +1,7 @@
 package GitHubPracties;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,15 +10,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		driver.get("https://www.google.com/");
 		WebElement Textbox = driver.findElement(By.name("q"));
 		Textbox.sendKeys("amazon");
 		Textbox.submit();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@class='x2VHCd OSrXXb qzEoUe']")).click();
 		driver.close();
 	}
 }
