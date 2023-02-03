03ecad346620c5745f111eaa4b8e4616a3edc7b0
ppatil24 <ppatil24@INLEN8520062127.corp.capgemini.com>
2023-02-03 11:14:14 +0530

    Write simple code

--- src/main/java/GitHubPracties/Test.java
+++ src/main/java/GitHubPracties/Test.java
@@ -1,5 +1,22 @@
 package GitHubPracties;
 
-public class Test {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		WebElement Textbox = driver.findElement(By.name("q"));
+		Textbox.sendKeys("amazon");
+		Textbox.submit();
+		driver.close();
+	}
 }
